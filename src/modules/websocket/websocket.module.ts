4ff6222ef0e4bde8dac8cecb926bcf3e5d97687c
Huy Nguyen <huynh@missingcorner.com>
2025-04-08 15:03:38 +0700

    fix: import error

--- src/modules/websocket/websocket.module.ts
+++ src/modules/websocket/websocket.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ChatGateway } from './chat.gateway';
-import { ChatService } from './chat.service';
 import { ChatModule } from '../chat/chat.module';
+import { ChatService } from './chat.service';
 
 @Module({
   imports: [ChatModule],

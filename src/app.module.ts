41b430e1f77d12398ae0a75af963a0dafed2602d
Huy Nguyen <huynh@missingcorner.com>
2025-04-16 17:26:58 +0700

    chore: rename coordinator agent

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,7 @@ import { AppService } from './app.service';
 import { InfrastructureModule } from './shared/infrastructure/infrastructure.module';
 import { ConfigModule } from './config/config.module';
 import { ChatModule } from './modules/chat/chat.module';
-import { CoordinatorModule } from './modules/agents/coordinator/coordinator.module';
+import { CoordinatorModule } from './modules/agents/coordinator-agent/coordinator.module';
 import { SharedModule } from './shared/shared.module';
 import { ChatSessionModule } from './modules/chat-session/chat-session.module';
 import { EventEmitterModule } from '@nestjs/event-emitter';
